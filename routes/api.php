11af951bcb660ccc1e54653e5b6a59e4ecdbbfa7
uxamatahir8 <uxamatahir8@gmail.com>
2025-01-29 19:24:13 +0500

    issue

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,4 @@
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index']);
